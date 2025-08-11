781c38a6c5326af98fc69e1bba60eea02ea195ec
Rajdeep Todkar <rajdeeptodkar@gmail.com>
2025-08-11 13:03:14 +0530

    Disable submit button during loading and update button styles for better user feedback

--- app/page.tsx
+++ app/page.tsx
@@ -513,8 +513,9 @@ const MealPlanPersonalization = () => {
                   {/* Submit Button */}
                   <div className="flex px-4 py-3 justify-center">
                     <button
+                      disabled={onLoading}
                       type="submit"
-                      className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-blue-500 text-white text-sm font-bold leading-normal tracking-[0.015em] hover:bg-blue-700 transition-colors"
+                      className= {`flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4  text-white text-sm font-bold leading-normal tracking-[0.015em] ${onLoading ? "bg-gray-400" : "bg-blue-500 hover:bg-blue-700" }  transition-colors`}
                     >
                       <span className="truncate">Create</span>
                     </button>
